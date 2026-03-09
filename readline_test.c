8f68a3b95d31713db011f1a7b501fb4e3d951560
rapohlen <rapohlen@f1br6s4.paris.42.school>
2026-03-09 16:16:08 +0100

    Few more tests added (stat, readdir..)

--- readline_test.c
+++ readline_test.c
@@ -5,19 +5,67 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #define BUFFER_SIZE	64
 
+static void	print_stats(char *file, struct stat stats)
+{
+	printf("Stats of %s:\n", file);
+	printf("\tID of device containing file: %ld\n", stats.st_dev);
+	printf("\tInode number: %ld\n", stats.st_ino);
+	printf("\tFile type and mode: %d\n", stats.st_mode);
+	printf("\tNumber of hard links: %ld\n", stats.st_nlink);
+	printf("\tUser ID of owner: %d\n", stats.st_uid);
+	printf("\tGroup ID of owner: %d\n", stats.st_gid);
+	printf("\tDevice ID (if special file): %ld\n", stats.st_rdev);
+	printf("\tTotal size, in bytes: %ld\n", stats.st_size);
+	printf("\tBlock size for filesystem I/O: %ld\n", stats.st_blksize);
+	printf("\tNumber of 512 B blocks allocated: %ld\n", stats.st_blocks);
+}
+
+static void	read_dir_contents(char *dirname)
+{
+	DIR				*dirp;
+	struct dirent	*dirent;
+
+	dirp = opendir(dirname);
+	if (!dirp)
+		printf("%s: %s\n", dirname, strerror(errno));
+	else
+	{
+		while (1)
+		{
+			dirent = readdir(dirp);
+			if (!dirent)
+			{
+				if (errno)
+					printf("%s: %s\n", dirname, strerror(errno));
+				break ;
+			}
+			printf("%s\n", dirent->d_name);
+		}
+		closedir(dirp);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	char	buffer[BUFFER_SIZE];
-	char	*line;
+	struct stat	stats;
+	char		buffer[BUFFER_SIZE];
+	char		*line;
 
 	(void)ac;
 	while (1)
 	{
 		line = readline(av[1]);
-		if (!strcmp(line, "clear"))
+		if (!line || !strcmp(line, "exit"))
+		{
+			free(line);
+			return (0);
+		}
+		else if (!strcmp(line, "clear"))
 		{
 			printf("History cleared\n");
 			rl_clear_history();
@@ -32,14 +80,37 @@ int	main(int ac, char **av)
 		}
 		else if (!strncmp(line, "cd ", 3) && line[3])
 		{
+			add_history(line);
 			if (chdir(line + 3) == -1)
 				printf("%s: %s\n", line + 3, strerror(errno));
 			free(line);
 		}
-		else if (!line || !strcmp(line, "exit"))
+		else if (!strncmp(line, "stat ", 5) && line[5])
 		{
+			add_history(line);
+			if (stat(line + 5, &stats) == -1)
+				printf("%s: %s\n", line + 5, strerror(errno));
+			else
+				print_stats(line + 5, stats);
 			free(line);
-			return (0);
+		}
+		else if (!strncmp(line, "lstat ", 6) && line[6])
+		{
+			add_history(line);
+			if (lstat(line + 6, &stats) == -1)
+				printf("%s: %s\n", line + 6, strerror(errno));
+			else
+				print_stats(line + 6, stats);
+			free(line);
+		}
+		else if (!strcmp(line, "ls") || !strncmp(line, "ls ", 3))
+		{
+			add_history(line);
+			if (!line[2] || !line[3])
+				read_dir_contents(".");
+			else
+				read_dir_contents(line + 3);
+			free(line);
 		}
 		else
 		{
