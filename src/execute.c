3ab789e547d729061348feab568d5529da0845b0
Bastien Demouge <bdemouge@f4r13s7.clusters.42paris.fr>
2026-03-18 18:17:22 +0100

    gestion des pipes pour l'execution

--- src/execute.c
+++ src/execute.c
@@ -6,12 +6,14 @@
 /*   By: bdemouge <bdemouge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 15:30:37 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/03/18 16:06:53 by bdemouge         ###   ########.fr       */
+/*   Updated: 2026/03/18 18:16:51 by bdemouge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// UTILS
+//==========================================================
 int count_cmd(t_command *cmd)
 {
 	int i;
@@ -25,6 +27,17 @@ int count_cmd(t_command *cmd)
 	return (1);
 }
 
+void safe_close(int fd)
+{
+	if (fd == -1)
+		return ;
+	close(fd);
+	fd = -1;
+}
+//========================================================
+
+// GESTION PIPES
+//========================================================
 void clear_pipes(int **pipe_fd, int nb_pipes)
 {
 	int i;
@@ -32,14 +45,13 @@ void clear_pipes(int **pipe_fd, int nb_pipes)
 	i = 0;
 	while (i < nb_pipes)
 	{
-		close(pipe_fd[i][0]);
-		close(pipe_fd[i][1]);
+		safe_close(pipe_fd[i][0]);
+		safe_close(pipe_fd[i][1]);
 		free(pipe_fd[i]);
 		i++;
 	}
 	free(pipe_fd);
 }
-
 int **create_pipes(int nb_pipes)
 {
 	int **pipe_fd;
@@ -47,7 +59,7 @@ int **create_pipes(int nb_pipes)
 	
 	pipe_fd = malloc(sizeof(int *) * nb_pipes);
 	if (!pipe_fd)
-		return (NULL); //gerer erreur
+		return (perror("malloc"), NULL); //gerer erreur
 	i = 0;
 	while (i < nb_pipes)
 	{
@@ -57,6 +69,8 @@ int **create_pipes(int nb_pipes)
 			clear_pipes(pipe_fd, i);
 			return (NULL);
 		}
+		pipe_fd[i][0] = -1;
+		pipe_fd[i][1] = -1;
 		if(pipe(pipe_fd[i]) == -1)
 		{
 			clear_pipes(pipe_fd, i);
@@ -67,35 +81,62 @@ int **create_pipes(int nb_pipes)
 	return (pipe_fd);
 }
 
+void	handle_pipes(int **pipe_fd, int nb_cmd, int idx)
+{
+	if (idx == 0 && nb_cmd > 1)
+	{
+		safe_close(pipe_fd[idx][0]);
+		dup2(pipe_fd[idx][1], STDOUT_FILENO);
+		safe_close(pipe_fd[idx][1]);
+	}
+	else if(idx == nb_cmd - 1)
+	{
+		safe_close(pipe_fd[idx - 1][1]);
+		dup2(pipe_fd[idx - 1][0], STDIN_FILENO);
+		safe_close(pipe_fd[idx - 1][0]);
+	}
+	else
+	{
+		safe_close(pipe_fd[idx - 1][1]);
+		safe_close(pipe_fd[idx][0]);
+		dup2(pipe_fd[idx - 1][0], STDIN_FILENO);
+		safe_close(pipe_fd[idx - 1][0]);
+		dup2(pipe_fd[idx][1], STDOUT_FILENO);
+		safe_close(pipe_fd[idx][1]);
+	}
+}
+//========================================================
+
 int	execute(t_minishell *data)
 {
 	int			**pipe_fd;
+	int			nb_cmd;
+	int 		idx;
 	pid_t		pid;
 	t_command	*cmd;
 
-	// cmd = data->cmd_list;
-	// if (!cmd)
-	// 	return (0);
-	// while (cmd)
-	// {
-	// 	if (cmd->next)
-	// 	{
-	// 		if(pipe(pipe_fd) == -1)
-	// 			return (perror("pipe"), 0);
-	// 	}
-	// 	pid = fork();
-	// 	if (pid == -1)
-	// 	{
-	// 		perror("fork");
-	// 		return (0);
-	// 	}
-	// 	if (pid == 0)
-	// 	{
-	// 		if (cmd->redir == NULL)
-	// 		{
-	// 			close(pipe_fd[0])
-	// 		}
-	// 	}
-	// }
-	return (0);
+	cmd = data->cmd_list;
+	if (!cmd)
+		return (0);
+	nb_cmd = count_cmd(cmd);
+	pipe_fd = create_pipes(nb_cmd - 1);
+	if (!pipe_fd)
+		return (0);
+	idx = 0;
+	while (cmd)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			clear_pipes(pipe_fd, nb_cmd - 1);
+			return (0);
+		}
+		if (pid == 0)
+		{
+			handle_pipes(pipe_fd, nb_cmd, idx);
+		}
+		cmd = cmd->next;
+		idx++;
+	}
+	return (1);
 }
