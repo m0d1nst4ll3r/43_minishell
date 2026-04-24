c5650571565129efb71e4f399b993ec188bba5ba
rapohlen <rapohlen@f1r11s13.paris.42.school>
2026-04-24 14:45:58 +0200

    Added -nnn functionality to echo

--- src/echo.c
+++ src/echo.c
@@ -6,30 +6,47 @@
 /*   By: bdemouge <bdemouge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 18:49:44 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/03/24 11:39:06 by bdemouge         ###   ########.fr       */
+/*   Updated: 2026/04/24 14:45:04 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	is_option(char *str)
+{
+	int	i;
+
+	if (str[0] != '-' || !str[1])
+		return (0);
+	i = 1;
+	while (str[i])
+		if (str[i++] != 'n')
+			return (0);
+	return (1);
+}
+
 int	builtin_echo(int ac, char **av, char **ep)
 {
 	int	option_n;
+	int	i;
 
+	(void)ac;
 	(void)ep;
 	option_n = 0;
-	if (ac > 1 && !ft_strcmp(av[1], "-n"))
+	i = 1;
+	while (av[i])
 	{
-		av++;
-		ac--;
-		option_n = 1;
+		if (is_option(av[i]))
+			option_n = 1;
+		else
+			break ;
+		i++;
 	}
-	while (--ac)
+	while (av[i])
 	{
-		printf("%s", av[1]);
-		if (ac > 1)
+		printf("%s", av[i]);
+		if (av[++i])
 			printf(" ");
-		av++;
 	}
 	if (!option_n)
 		printf("\n");
